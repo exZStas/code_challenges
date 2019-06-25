813eea1125198b29c9c85b96255fac99eb643d41
sslavin <sslavin@promedico.nl>
2019-06-25 12:48:24 +0700

    Initial commit

---
+++ src/main/java/ru/exzstas/code/challenges/CodeChallangesApplication.java
@@ -0,0 +1,13 @@
+package ru.exzstas.code.challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallangesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeChallangesApplication.class, args);
+    }
+
+}
