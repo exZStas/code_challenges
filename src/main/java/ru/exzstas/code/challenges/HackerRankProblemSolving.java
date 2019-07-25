2e9f7484e1addd02ed34b0182c56a647f7490b67
sslavin <sslavin@promedico.nl>
2019-07-25 10:47:22 +0700

    hackerrank apple and orange challenge

--- src/main/java/ru/exzstas/code/challenges/HackerRankProblemSolving.java
+++ src/main/java/ru/exzstas/code/challenges/HackerRankProblemSolving.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class HackerRankProblemSolving {
 
     public static void main(String[] args) {
-        System.out.println(gradeStudents(Arrays.asList(4, 73, 67, 38, 33)));
+        countApplesAndOranges(7, 11, 5, 15, new int[] {-2, 2, 1}, new int[] {5, -6});
     }
 
     //https://www.hackerrank.com/challenges/grading/problem
@@ -29,5 +29,25 @@ public class HackerRankProblemSolving {
         });
 
         return result;
+    }
+
+    //https://www.hackerrank.com/challenges/apple-and-orange/problem
+    public static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int houseStart = s;
+        int houseEnd = t;
+
+        int appleTree = a;
+        int orangeTree = b;
+
+        long appleRes = Arrays.stream(apples).boxed()
+                .filter(appleDist -> appleDist > 0)
+                .filter(appleDist -> appleTree + appleDist >= houseStart && appleTree + appleDist <= houseEnd).count();
+
+        long orangeRes = Arrays.stream(oranges).boxed()
+                .filter(orangeDist -> orangeDist < 0)
+                .filter(orangeDist -> orangeTree + orangeDist <= houseEnd && orangeTree + orangeDist >= houseStart).count();
+
+        System.out.println(appleRes);
+        System.out.print(orangeRes);
     }
 }
